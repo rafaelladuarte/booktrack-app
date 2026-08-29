3a741e145658a857f2e4eb28cbb9e299c8456a56
Rafaella Duarte <rafaella.d.d.carvalho@gmail.com>
2026-08-29 16:31:58 -0300

    feat: update STATIC_URL to include leading slash and add STATIC_ROOT for static files

--- config/settings.py
+++ config/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # URL da API Backend
 API_BASE_URL = os.getenv('API_BASE_URL', 'http://127.0.0.1:8000')
